7fd41481ebe570083d29f831d5be7599055c7edf
Sinh Hoang <lehoangsinhhn99@gmail.com>
2022-03-10 11:34:54 +0700

    refactor with reused widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:simple_app/Screen/Home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:simple_app/Utils/debouncer.dart';
-
-import 'Screen/login_button.dart';
+import 'package:simple_app/commom/widgets/stateless/lhs_button.dart';
+import 'package:simple_app/commom/widgets/stateless/lhs_text_form_field.dart';
+import 'Utils/debouncer.dart';
 
 void main() {
   showLayoutGuidelines();
@@ -27,15 +27,15 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   FocusNode nodeOne = FocusNode();
-
   FocusNode nodeTwo = FocusNode();
+
+  // Form
+  final _formKey = GlobalKey<FormState>();
+  final _userNameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   bool _isLogin = true;
   LoadingState _loginState = LoadingState.finished;
-
-  String _userName = "";
-
-  String _password = "";
 
   @override
   Widget build(BuildContext context) {
@@ -49,18 +49,8 @@ class _MyAppState extends State<MyApp> {
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 children: [
-                  SizedBox(
-                    width: double.infinity,
-                    height: 100,
-                    child: _header,
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: SingleChildScrollView(
-                        child: _createContent(context),
-                      ),
-                    ),
-                  ),
+                  _header(),
+                  _createContent(),
                 ],
               ),
             ),
@@ -76,14 +66,20 @@ class _MyAppState extends State<MyApp> {
     title: const Text('App bar'),
   );
 
-  final Container _header = Container(
-    color: Colors.red,
-    alignment: AlignmentDirectional.center,
-    child: const Text(
-      'MoneyLover 4',
-      style: TextStyle(color: Colors.white),
-    ),
-  );
+  Widget _header() {
+    return SizedBox(
+      width: double.infinity,
+      height: 100,
+      child: Container(
+        color: Colors.red,
+        alignment: AlignmentDirectional.center,
+        child: const Text(
+          'MoneyLover 4',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
 
   Widget _createIndicator(BuildContext context) {
     switch (_loginState) {
@@ -93,7 +89,7 @@ class _MyAppState extends State<MyApp> {
             width: double.infinity,
             height: double.infinity,
             color: Colors.transparent,
-            child: CupertinoActivityIndicator(radius: 20),
+            child: const CupertinoActivityIndicator(radius: 20),
           ),
         );
       default:
@@ -101,91 +97,104 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  Widget _createContent(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 48),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          _createTextField(TextFieldType.userName, context),
-          const SizedBox(height: 16),
-          _createTextField(TextFieldType.password, context),
-          const SizedBox(height: 8),
-          _createForgetPasswordButton(),
-          const SizedBox(height: 8),
-          LoginButton(
-              onPress: () {
-                print("Tap");
-                onPress(context);
-              },
-              isLogin: _isLogin),
-          const SizedBox(height: 8),
-          Row(
-            children: [
-              GestureDetector(
-                child: RichText(
-                  text: TextSpan(
-                    text: _isLogin
-                        ? "Don't have account, "
-                        : "Already have account, ",
-                    style: const TextStyle(fontSize: 14, color: Colors.black),
-                    children: [
-                      TextSpan(
-                        text: _isLogin ? 'Register' : 'Login',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            color: Colors.green),
-                      ),
-                    ],
-                  ),
-                ),
-                onTap: () {
-                  setState(() {
-                    _isLogin = !_isLogin;
-                  });
-                },
+  Widget _createContent() {
+    return Expanded(
+      child: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  _userTextFormField(),
+                  const SizedBox(height: 16),
+                  _passwordTextFormField(),
+                  const SizedBox(height: 8),
+                  _createForgetPasswordButton(),
+                  const SizedBox(height: 8),
+                  _loginButton(),
+                  const SizedBox(height: 8),
+                  _register(),
+                ],
               ),
-            ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
 
-  TextField _createTextField(TextFieldType type, BuildContext context) {
-    return TextField(
-      focusNode: type == TextFieldType.userName ? nodeOne : nodeTwo,
-      decoration: InputDecoration(
-        prefixIcon: Icon(
-          type == TextFieldType.userName ? Icons.people : Icons.security,
-        ),
-        labelText: TextFieldType.userName == type ? 'User name' : 'Password',
-        // labelStyle: const TextStyle(color: Colors.red),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.black),
-        ),
-        contentPadding: const EdgeInsets.only(left: 16),
-      ),
-      style: const TextStyle(color: Colors.blue),
-      onSubmitted: (value) {
-        if (type == TextFieldType.userName) {
-          FocusScope.of(context).requestFocus(nodeTwo);
-        }
+  LHSTextFormField _userTextFormField() {
+    return LHSTextFormField(
+      controller: _userNameController,
+      focusNode: nodeOne,
+      prefix: const Icon(Icons.people),
+      label: "User Name",
+      obsecure: false,
+      onEditingCompleted: () {
+        FocusScope.of(context).requestFocus(nodeTwo);
+      },
+      validator: (text) {
+        return null;
+      },
+    );
+  }
+
+  LHSTextFormField _passwordTextFormField() {
+    return LHSTextFormField(
+      controller: _passwordController,
+      focusNode: nodeTwo,
+      prefix: const Icon(Icons.security),
+      label: "Password",
+      obsecure: false,
+      // onChanged: (text) => _password = text,
+      onEditingCompleted: () {
         onPress(context);
       },
-      obscureText: type == TextFieldType.password ? true : false,
-      onChanged: (value) {
-        if (type == TextFieldType.userName) {
-          _userName = value;
-        } else {
-          _password = value;
-        }
+      validator: (text) {
+        return null;
       },
+    );
+  }
+
+  LHSButton _loginButton() {
+    return LHSButton(
+        onPress: () {
+          onPress(context);
+        },
+        isLogin: _isLogin);
+  }
+
+  Row _register() {
+    return Row(
+      children: [
+        GestureDetector(
+          child: RichText(
+            text: TextSpan(
+              text:
+                  _isLogin ? "Don't have account, " : "Already have account, ",
+              style: const TextStyle(fontSize: 14, color: Colors.black),
+              children: [
+                TextSpan(
+                  text: _isLogin ? 'Register' : 'Login',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: Colors.green),
+                ),
+              ],
+            ),
+          ),
+          onTap: () {
+            setState(() {
+              _isLogin = !_isLogin;
+            });
+          },
+        ),
+      ],
     );
   }
 
@@ -203,20 +212,22 @@ class _MyAppState extends State<MyApp> {
   }
 
   void onPress(BuildContext context) async {
-    if (_userName.isEmpty && _password.isEmpty) {
-      Utils.showError(context, 'Warning', 'user name or password is empty');
+    if (!_formKey.currentState!.validate()) {
+      Utils.showError(context, 'Warning', 'user name or password is invalid');
       return;
     }
     setState(() {
       _loginState = LoadingState.loading;
     });
+    final userName = _userNameController.text;
+    final password = _passwordController.text;
     try {
       if (!_isLogin) {
         await FirebaseAuth.instance.createUserWithEmailAndPassword(
-            email: _userName, password: _password);
+            email: userName, password: password);
       }
       await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: _userName, password: _password);
+          .signInWithEmailAndPassword(email: userName, password: password);
     } on PlatformException catch (e) {
       Utils.showError(context, e.code, e.message.toString());
       setState(() {
@@ -239,5 +250,4 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-enum TextFieldType { userName, password }
 enum LoadingState { loading, finished }
