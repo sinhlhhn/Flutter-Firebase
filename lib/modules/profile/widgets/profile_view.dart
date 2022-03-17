9a9f1ab08981dc5abc4f80a580c0b8884d24e8a1
Sinh Hoang <lehoangsinhhn99@gmail.com>
2022-03-17 14:00:28 +0700

    profile view

--- lib/modules/profile/widgets/profile_view.dart
+++ lib/modules/profile/widgets/profile_view.dart
@@ -1,77 +1,112 @@
-import 'dart:io';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:simple_app/commom/widgets/stateless/lhs_indicator.dart';
 import 'package:simple_app/modules/authentication/bloc/bloc/authentication_bloc.dart';
 import 'package:simple_app/modules/profile/bloc/profile_bloc.dart';
+import 'package:simple_app/utils/dart.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    context.read<ProfileBloc>().add(const GetCurrentAvatar());
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            BlocConsumer<ProfileBloc, ProfileState>(
-              listener: (context, state) {
-                if (state.error.isNotEmpty) {
-                  ScaffoldMessenger.of(context)
-                    ..hideCurrentSnackBar()
-                    ..showSnackBar(
-                      SnackBar(content: Text(state.error)),
-                    );
-                }
-              },
-              builder: (context, state) {
-                final imagePath = state.imagePath;
-                return CircleAvatar(
-                  radius: 50,
-                  backgroundImage: _getBackgroundImage(imagePath),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Image picker'),
-              onPressed: () => showActionSheet(context),
-            ),
-            ElevatedButton(
-              child: const Text('Logout'),
-              onPressed: () {
-                context
-                    .read<AuthenticationBloc>()
-                    .add(AuthenticationLogoutRequested());
-              },
-            ),
-          ],
+      appBar: _appBar(context),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              addVerticalSpace(50),
+              _avatar(),
+              _changeAvatarButton(context),
+              _email(context),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _getUser(BuildContext context) {
-    final userId = context.select(
-      (AuthenticationBloc bloc) => bloc.state.user.id,
+  AppBar _appBar(BuildContext context) {
+    return AppBar(
+      title: const Text('Home'),
+      actions: [
+        IconButton(
+          onPressed: () {
+            context
+                .read<AuthenticationBloc>()
+                .add(AuthenticationLogoutRequested());
+          },
+          icon: const Icon(Icons.logout),
+        )
+      ],
     );
+  }
+
+  Widget _avatar() {
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      listener: (context, state) {
+        if (state.error.isNotEmpty) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text(state.error)),
+            );
+        }
+      },
+      builder: (context, state) {
+        final imagePath = state.imageUrl;
+        if (imagePath.isEmpty) {
+          return CircleAvatar(
+            radius: 75,
+            backgroundColor: Colors.grey[200],
+          );
+        }
+        return SizedBox(
+          width: 150,
+          height: 150,
+          child: CachedNetworkImage(
+            imageUrl: imagePath,
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(75),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            placeholder: (context, url) => const LHSIndicator(),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _changeAvatarButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextButton(
+        child: const Text('Change Avatar'),
+        onPressed: () => showActionSheet(context),
+      ),
+    );
+  }
+
+  Widget _email(BuildContext context) {
     final bloc = context.read<AuthenticationBloc>();
     final user = bloc.state.user;
     final email = user.email;
-    return Text('$userId - $email');
-  }
-
-  ImageProvider<Object>? _getBackgroundImage(String imagePath) {
-    if (imagePath.isNotEmpty) {
-      print(imagePath);
-      return FileImage(File(imagePath));
-    } else {
-      return null;
-    }
+    return ListTile(
+      tileColor: Colors.grey,
+      title: Text(email ?? ""),
+      leading: const Icon(Icons.email),
+    );
   }
 
   void showActionSheet(BuildContext context) {
@@ -102,6 +137,4 @@ class ProfileView extends StatelessWidget {
           );
         });
   }
-
- 
 }
