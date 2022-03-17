bb65175dc6da57d17e966aaad80b37ffa6c0255d
Sinh Hoang <lehoangsinhhn99@gmail.com>
2022-03-17 16:16:35 +0700

    deny rebuild when select TabBarView

--- lib/modules/profile/widgets/profile_page.dart
+++ lib/modules/profile/widgets/profile_page.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:simple_app/modules/profile/bloc/profile_bloc.dart';
 
 import 'profile_view.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
   static Route route() {
     return MaterialPageRoute<void>(builder: (_) => const ProfilePage());
   }
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage>
+    with AutomaticKeepAliveClientMixin {
+  /// Giúp cho `Widget` này không `rebuild` khi chuyển `tab`
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return BlocProvider(
       create: (context) => ProfileBloc(),
       child: const ProfileView(),
