5ddae4d86dafab6322a250f6fc5bf460ccb52ba0
Sinh Hoang <lehoangsinhhn99@gmail.com>
2022-03-20 22:44:01 +0700

    refactor movie model

--- lib/modules/movie/models/movie_model.dart
+++ lib/modules/movie/models/movie_model.dart
@@ -1,32 +1,82 @@
 import 'package:equatable/equatable.dart';
 
 // Dữ liệu cuối cùng được sử dụng để hiển thị lên UI
-class Movie extends Equatable {
-  final int? id;
-  final num? voteAverage;
-  final String? title;
-  final String? posterPath;
-  final String? overview;
-  final String? releaseDate;
 
-  const Movie(
-    this.id,
-    this.voteAverage,
-    this.title,
-    this.posterPath,
-    this.overview,
-    this.releaseDate,
-  );
+class Movie extends Equatable {
+  int? voteCount;
+  int? id;
+  bool? video;
+  dynamic voteAverage;
+  String? title;
+  double? popularity;
+  String? posterPath;
+  String? originalLanguage;
+  String? originalTitle;
+  List<int>? genreIds;
+  String? backdropPath;
+  bool? adult;
+  String? overview;
+  String? releaseDate;
+
+  Movie(
+      {this.voteCount,
+      this.id,
+      this.video,
+      this.voteAverage,
+      this.title,
+      this.popularity,
+      this.posterPath,
+      this.originalLanguage,
+      this.originalTitle,
+      this.genreIds,
+      this.backdropPath,
+      this.adult,
+      this.overview,
+      this.releaseDate});
+
+  String get posterUrl {
+    return "https://image.tmdb.org/t/p/w342" + (posterPath ?? "");
+  }
+
+  String get backdropUrl {
+    return "https://image.tmdb.org/t/p/w342" + (backdropPath ?? "");
+  }
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      json['id'],
-      json['vote_average'],
-      json['title'],
-      json['poster_path'],
-      json['overview'],
-      json['release_date'],
-    );
+        voteCount: json['vote_count'],
+        id: json['id'],
+        video: json['video'],
+        voteAverage: json['vote_average'],
+        title: json['title'],
+        popularity: json['popularity'],
+        posterPath: json['poster_path'],
+        originalLanguage: json['original_language'],
+        originalTitle: json['original_title'],
+        genreIds: json['genre_ids'].cast<int>(),
+        backdropPath: json['backdrop_path'],
+        adult: json['adult'],
+        overview: json['overview'],
+        releaseDate: json['release_date']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['vote_count'] = voteCount;
+    data['id'] = id;
+    data['video'] = video;
+    data['vote_average'] = voteAverage;
+    data['title'] = title;
+    data['popularity'] = popularity;
+    data['poster_path'] = posterPath;
+    data['original_language'] = originalLanguage;
+    data['original_title'] = originalTitle;
+    data['genre_ids'] = genreIds;
+    data['backdrop_path'] = backdropPath;
+    data['adult'] = adult;
+    data['overview'] = overview;
+    data['release_date'] = releaseDate;
+    return data;
   }
 
   @override
